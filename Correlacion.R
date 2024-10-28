6b582227cc80114d282e8c28bf5c9072a4b731d1
unknown <7456049821@untrm.edu.pe>
2024-10-28 18:30:32 -0500

    Mensaje

---
+++ Correlacion.R
@@ -0,0 +1,19 @@
+# Definir los factores
+fertilizacion <- c(0, 50, 100)  # Dosis de fertilización nitrogenada (kg/ha)
+cultivares <- c("Peruanita", "Canchan")  # Cultivares de papa
+
+# Crear una combinación de todos los tratamientos
+tratamientos <- expand.grid(Fertilizacion = fertilizacion, Cultivar = cultivares)
+
+# Definir el número de bloques (repeticiones)
+repeticiones <- 4
+
+# Crear el diseño en bloques completamente al azar (DBCA)
+diseño_dbca <- design.ab(rcbd, treatments=list(fertilizacion, cultivares), r=repeticiones, serie=2)
+
+# Mostrar el diseño experimental
+print(diseño_dbca$book)
+
+# Graficar el diseño experimental
+plot(diseño_dbca)
+
